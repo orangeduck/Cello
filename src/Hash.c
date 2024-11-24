d5346eb8e63346bd8a7f1743af9657b47c1032bb
Bill Morgan <arthurwilliammorgan@gmail.com>
2024-11-24 10:46:15 -0600

    fix undefined behavior in hash_data
    .\src/Hash.c:89:16: runtime error: load of misaligned address 0x7ff6d8a04435 for type 'const uint64_t' (aka 'const unsigned long long'), which requires 8 byte alignment
    0x7ff6d8a04435: note: pointer points here
     67 65 2e 00 5f 5f 45  78 63 65 70 74 69 6f 6e  00 3c 27 45 78 63 65 70  74 69 6f 6e 27 20 41 74  20
                 ^
    SUMMARY: UndefinedBehaviorSanitizer: undefined-behavior .\src/Hash.c:89:16

--- src/Hash.c
+++ src/Hash.c
@@ -80,13 +80,15 @@ uint64_t hash_data(const void* data, size_t size) {
   
   const uint64_t m = 0xc6a4a7935bd1e995;
 	const int r = 47;
-	const uint64_t * d = (const uint64_t*)data;
-	const uint64_t * end = d + (size/8);
+	const uint8_t * d = (const uint8_t*)data;
+	const uint8_t * end = d + (size & ~7ULL);
   
 	uint64_t h = 0xCe110 ^ (size * m);
 
 	while (d != end) {
-		uint64_t k = *d++;
+		uint64_t k;
+		memcpy(&k, d, sizeof(uint64_t));
+		d += sizeof(uint64_t);
 		k *= m; 
 		k ^= k >> r; 
 		k *= m; 
@@ -94,16 +96,14 @@ uint64_t hash_data(const void* data, size_t size) {
 		h *= m; 
 	}
 
-	const unsigned char * data2 = (const unsigned char*)d;
-
 	switch (size & 7) {
-    case 7: h ^= (uint64_t)(data2[6]) << 48;
-    case 6: h ^= (uint64_t)(data2[5]) << 40;
-    case 5: h ^= (uint64_t)(data2[4]) << 32;
-    case 4: h ^= (uint64_t)(data2[3]) << 24;
-    case 3: h ^= (uint64_t)(data2[2]) << 16;
-    case 2: h ^= (uint64_t)(data2[1]) << 8;
-    case 1: h ^= (uint64_t)(data2[0]);
+    case 7: h ^= (uint64_t)(d[6]) << 48;
+    case 6: h ^= (uint64_t)(d[5]) << 40;
+    case 5: h ^= (uint64_t)(d[4]) << 32;
+    case 4: h ^= (uint64_t)(d[3]) << 24;
+    case 3: h ^= (uint64_t)(d[2]) << 16;
+    case 2: h ^= (uint64_t)(d[1]) << 8;
+    case 1: h ^= (uint64_t)(d[0]);
             h *= m;
 	};
   
